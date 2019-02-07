bd465cc817dc31278ac920e67dbed795dd17960e
Abhishek Balkrishna <abhishekbalkrishna4@gmail.com>
2019-02-07 14:39:38 +0000

    new

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,5 +1,7 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import datetime
+from floodsystem.analysis import polyfit
 def plot_water_levels(station, dates, levels):
    
     plt.plot(dates, levels)
@@ -9,4 +11,20 @@ def plot_water_levels(station, dates, levels):
     plt.title("Station:     {}\n".format(station.name))
     plt.tight_layout
     plt.show()
+    return "No Error"
+
+def plot_water_level_with_fit(station, dates, levels, p):
+    pol = polyfit(dates, levels, p)
+    x = []
+    y = []
+    for i in pol:
+        i[0].append(y)
+        i[1].append(x)
+    plt.plot(x, y)
+    plt.show()
+    xax = matplotlib.dates.date2num(dates)
+    plt.plot(xax, levels)
+    plt.show()
+
+     
         
