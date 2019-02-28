5952775e8d7836375c3310303439b599fff5ea63
Abhishek Balkrishna <abhishekbalkrishna4@gmail.com>
2019-02-28 14:22:36 +0000

    changes

--- Task2G.py
+++ Task2G.py
@@ -73,80 +73,10 @@ for station in threat_stations:
     else:
         low.append(station) 
  
-"""
- 
-for i in range(0, len(threat_stations)):
-    name = threat_stations[i][0]
-    for station in stations:
-        if station.name == name:
-            station_list.append((station, threat_stations[i][1]))
-            break
- 
-severe = station_list[:9]
-print("Severe:", [(station[0].name, station[0].relative_water_level()) for station in severe])
-high = []
-x = datetime.datetime.now()
- 
-y = matplotlib.dates.date2num(x) + 1
-remaining_stations = list()
-failed_stations = list()
- 
-for station in stations:
-    if station not in severe and station not in high:
-        remaining_stations.append(station)
- 
-for station in remaining_stations:
-    if station.typical_range_consistent():
-        dt = 2
-        try: 
-            dates0, levs0 = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days = dt))
-            pol, d0 = polyfit(dates0, levs0, 4)
-            #print (pol, d0, y-d0)
-            #print(pol(y - d0), station.typical_range[1])
-        except (IndexError, ValueError):
-            failed_stations.append(station.name)
-            print("Skipping a station")
-            continue
-     
-        pl = pol(y - d0)
-        relative = station.relative_water_level()
-        #rel_level(pl,station)
-    
-        if relative >= 0.5 and pl > station.latest_level:
-            high.append((station.name, relative )) 
-print("High:", high)  
 
-
-
-    
-" Moderate : in 2 days time relative level will be above 0.7 "
-" Low : in 5 days times relative level above 0.7"
-
-
-z = matplotlib.dates.date2num(x) + 2
+  
  
-moderate = []
-list_num = len(severe) + len(high)
-for station in station_list[list_num + 1:] :
-    station = station[0]
-    if station.typical_range_consistent():
-        dt = 2
-        dates_mod, levs_mod = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days = dt))
-        try:
-            pol_mod, d_mod = polyfit(dates_mod, levs_mod, 4)
-            #print (pol_mod, d_mod, z-d_mod)
-            #print(pol_mod(z - d_mod), station.typical_range[1])
-        except :
-            failed_stations.append(station.name)
-            print("Skipping a station")
-            continue
-        rising = rising_gradient(pol_mod, d_mod, z)
-        #rel_level(pol_mod(z - d_mod), station)
-         
-        if  rising == True and pol_mod(z - d_mod) > 0.8*station.typical_range[1]:
-            moderate.append((station.name, station.relative_water_level())) 
- 
-"""
+
  
 print("Severe:", [station.name for station in severe])
 print("High:", [station.name for station in high])
